22bf810be8bb7ee322d9f2b935e7b4ab82b40c6b
Aaron Weidele <>
2024-06-10 12:48:37 -0400

    9.160(b)

--- attachments/09 Practice Project - Project Management/01-starting-project/src/components/Tasks.jsx
+++ attachments/09 Practice Project - Project Management/01-starting-project/src/components/Tasks.jsx
@@ -1,20 +1,24 @@
 import Button from "./Button";
 import TaskList from "./TaskList";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Tasks({ tasks, onAddTask }) {
-  const newTask = useRef();
+  const [enteredTask, setEnteredTask] = useState();
+  function handleChange(event) {
+    setEnteredTask(event.target.value);
+  }
+
   function handleNewTask() {
     onAddTask({
-      name: newTask.current.value,
+      name: enteredTask,
     });
-    newTask.current.value = "";
+    setEnteredTask("");
   }
   return (
     <div class="my-8">
       <h2 class="font-bold text-xl text-slate-600 my-6">Tasks</h2>
       <div class="flex gap-4">
-        <input ref={newTask} className="block bg-slate-300 p-1 border-b-slate-400 border-b-2" />
+        <input value={enteredTask} onChange={handleChange} className="block bg-slate-300 p-1 border-b-slate-400 border-b-2" />
         <Button onClick={handleNewTask}>Add Task</Button>
       </div>
       <TaskList tasks={tasks} />
