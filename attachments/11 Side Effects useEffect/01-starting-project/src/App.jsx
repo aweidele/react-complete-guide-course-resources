5fef1aa40e464e77194a55b6b39f995a186d68d6
Aaron Weidele <>
2024-06-17 11:57:12 -0400

    11.178

--- attachments/11 Side Effects useEffect/01-starting-project/src/App.jsx
+++ attachments/11 Side Effects useEffect/01-starting-project/src/App.jsx
@@ -10,10 +10,12 @@ import { sortPlacesByDistance } from "./loc.js";
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
+  const [avalablePlaces, setAvailablePlaces] = useState([]);
   const [pickedPlaces, setPickedPlaces] = useState([]);
 
   navigator.geolocation.getCurrentPosition((position) => {
     const sortedPlaces = sortPlacesByDistance(AVAILABLE_PLACES, position.coords.latitude, position.coords.longitude);
+    setAvailablePlaces(sortedPlaces);
   });
 
   function handleStartRemovePlace(id) {
@@ -53,7 +55,7 @@ function App() {
       </header>
       <main>
         <Places title="I'd like to visit ..." fallbackText={"Select the places you would like to visit below."} places={pickedPlaces} onSelectPlace={handleStartRemovePlace} />
-        <Places title="Available Places" places={AVAILABLE_PLACES} onSelectPlace={handleSelectPlace} />
+        <Places title="Available Places" places={avalablePlaces} onSelectPlace={handleSelectPlace} />
       </main>
     </>
   );
