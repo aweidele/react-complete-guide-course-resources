1c91acb60539f2f66503461a776309008cc2bee3
Aaron Weidele <>
2024-06-21 11:49:40 -0400

    12.199

--- attachments/13 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
+++ attachments/13 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
@@ -1,30 +1,41 @@
 import { useState, useCallback } from "react";
 import QUESTIONS from "../questions";
-import quizCompleteImage from "../assets/quiz-complete.png";
 
 import QuestionTimer from "./QuestionTimer";
+import QuizComplete from "./QuizComplete";
 
 export default function Quiz() {
   // const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  const [answerState, setAnswerState] = useState("");
   const [userAnswers, setUserAnswer] = useState([]);
-  const activeQuestionIndex = userAnswers.length;
+  const activeQuestionIndex = answerState === "" ? userAnswers.length : userAnswers.length - 1;
   const quizOver = QUESTIONS.length === activeQuestionIndex;
 
-  const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
-    console.log("handleSelectAnswer()");
-    setUserAnswer((prevUserAnswers) => [...prevUserAnswers, selectedAnswer]);
-  }, []);
+  const handleSelectAnswer = useCallback(
+    function handleSelectAnswer(selectedAnswer) {
+      setAnswerState("answered");
+      setUserAnswer((prevUserAnswers) => [...prevUserAnswers, { userAnswer: selectedAnswer, correctAnswer: QUESTIONS[activeQuestionIndex].answers[0] }]);
+
+      setTimeout(() => {
+        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+          setAnswerState("correct");
+        } else {
+          setAnswerState("wrong");
+        }
+
+        setTimeout(() => {
+          setAnswerState("");
+        }, 2000);
+      }, 1000);
+    },
+    [activeQuestionIndex]
+  );
 
   const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
   // Return Quiz Over if the quiz is over
   if (quizOver) {
-    return (
-      <div id="summary">
-        <img src={quizCompleteImage} />
-        <h2>Quiz complete</h2>
-      </div>
-    );
+    return <QuizComplete results={userAnswers} />;
   }
 
   // Otherwise, continue
@@ -34,24 +45,34 @@ export default function Quiz() {
   return (
     <div id="quiz">
       <div id="question">
-        <QuestionTimer key={QUESTIONS[activeQuestionIndex].id} onTimeout={handleSkipAnswer} />
+        <QuestionTimer key={activeQuestionIndex} onTimeout={handleSkipAnswer} />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {shuffledAnswers.map((answer) => (
-            <li className="answer" key={answer}>
-              <button
-                onClick={() => {
-                  handleSelectAnswer(answer);
-                }}
-              >
-                {answer}
-              </button>
-            </li>
-          ))}
+          {shuffledAnswers.map((answer) => {
+            const isSelected = userAnswers[userAnswers.length - 1] === answer;
+            let cssClasses = "";
+            if (answerState === "answered" && isSelected) cssClasses = "selected";
+            if ((answerState === "correct" || answerState === "wrong") && isSelected) cssClasses = answerState;
+            // if (answerState === "correct") cssClasses = "correct";
+            // if (answerState === "wrong") cssClasses = "wrong";
+            return (
+              <li className="answer" key={answer}>
+                {/* {isSelected ? "selected" : "not selected"} */}
+                <button
+                  onClick={() => {
+                    handleSelectAnswer(answer);
+                  }}
+                  className={cssClasses}
+                >
+                  {answer}
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </div>
       {userAnswers.map((answer, key) => (
-        <div key={`answer-${key}`}>{answer === null ? "No answer given" : answer}</div>
+        <div key={`answer-${key}`}>{answer.userAnswer === null ? "No answer given" : answer.userAnswer}</div>
       ))}
     </div>
   );
---
+++ attachments/13 Demo Project - React Quiz/01-starting-project/src/components/QuizComplete.jsx
@@ -0,0 +1,18 @@
+import quizCompleteImage from "../assets/quiz-complete.png";
+
+export default function QuizComplete({ results }) {
+  return (
+    <div id="summary">
+      <img src={quizCompleteImage} />
+      <h2>Quiz complete</h2>
+      <ul>
+        {results.map((result) => (
+          <li>
+            <div>{result.userAnswer}</div>
+            {/* <div>{result.correctAnswer}</div> */}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
