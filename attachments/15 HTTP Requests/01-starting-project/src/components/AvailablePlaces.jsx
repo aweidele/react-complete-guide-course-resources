aa4ce85c23b9fec3707739e8460a9077968bb60b
Aaron Weidele <aweidele@gmail.com>
2024-12-13 10:54:16 -0500

    15.237

--- attachments/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ attachments/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 import ErrorPage from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
@@ -19,12 +20,15 @@ export default function AvailablePlaces({ onSelectPlace }) {
           throw new Error("Failed to fetch places");
         }
 
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(resData.places, position.coords.latitude, position.coords.longitude);
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
         setError({ message: error.message || "could not fetch places, please try again later" });
+        setIsFetching(false);
       }
-
-      setIsFetching(false);
     }
 
     fetchPlaces();
