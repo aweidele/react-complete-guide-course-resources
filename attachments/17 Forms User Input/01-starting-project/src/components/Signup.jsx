9f94bfdf9f264a13cade56acab1b2da47eb8720f
Aaron Weidele <aweidele@gmail.com>
2024-12-16 23:26:01 -0500

    17.256

--- attachments/17 Forms User Input/01-starting-project/src/components/Signup.jsx
+++ attachments/17 Forms User Input/01-starting-project/src/components/Signup.jsx
@@ -1,6 +1,14 @@
 export default function Signup() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const fd = new FormData(event.target);
+    const acquisitionChannel = fd.getAll("acquisition"); // For checkboxes
+    const values = { ...Object.fromEntries(fd.entries()), acquisition: acquisitionChannel };
+    console.log(values);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
@@ -17,11 +25,7 @@ export default function Signup() {
 
         <div className="control">
           <label htmlFor="confirm-password">Confirm Password</label>
-          <input
-            id="confirm-password"
-            type="password"
-            name="confirm-password"
-          />
+          <input id="confirm-password" type="password" name="confirm-password" />
         </div>
       </div>
 
@@ -53,22 +57,12 @@ export default function Signup() {
       <fieldset>
         <legend>How did you find us?</legend>
         <div className="control">
-          <input
-            type="checkbox"
-            id="google"
-            name="acquisition"
-            value="google"
-          />
+          <input type="checkbox" id="google" name="acquisition" value="google" />
           <label htmlFor="google">Google</label>
         </div>
 
         <div className="control">
-          <input
-            type="checkbox"
-            id="friend"
-            name="acquisition"
-            value="friend"
-          />
+          <input type="checkbox" id="friend" name="acquisition" value="friend" />
           <label htmlFor="friend">Referred by friend</label>
         </div>
 
@@ -80,8 +74,7 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
-          agree to the terms and conditions
+          <input type="checkbox" id="terms-and-conditions" name="terms" />I agree to the terms and conditions
         </label>
       </div>
 
