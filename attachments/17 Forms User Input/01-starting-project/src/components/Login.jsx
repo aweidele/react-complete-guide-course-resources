56419d158e82d2d19285e377d443bca7f819156e
Aaron Weidele <aweidele@gmail.com>
2024-12-16 22:40:43 -0500

    Revert "17.255"
    This reverts commit c2513c94ecbe08991922283f57d43d9a88919b29.

--- attachments/17 Forms User Input/01-starting-project/src/components/Login.jsx
+++ attachments/17 Forms User Input/01-starting-project/src/components/Login.jsx
@@ -1,29 +1,17 @@
-import { useRef } from "react";
-
 export default function Login() {
-  const email = useRef();
-  const password = useRef();
-
-  function handleSumbit(event) {
-    event.preventDefault();
-    const enteredEmail = email.current.value;
-    const enteredPassword = password.current.value;
-
-    alert(`${enteredEmail} / ${enteredPassword}`);
-  }
   return (
-    <form onSubmit={handleSumbit}>
+    <form>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" ref={email} />
+          <input id="email" type="email" name="email" />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" ref={password} />
+          <input id="password" type="password" name="password" />
         </div>
       </div>
 
