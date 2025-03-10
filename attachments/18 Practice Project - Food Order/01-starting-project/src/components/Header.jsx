772173d9fad76ddf0856fbce6c8baf295796b292
Aaron Weidele <aweidele@gmail.com>
2025-03-10 14:45:09 -0400

    19.287

--- attachments/18 Practice Project - Food Order/01-starting-project/src/components/Header.jsx
+++ attachments/18 Practice Project - Food Order/01-starting-project/src/components/Header.jsx
@@ -1,13 +1,14 @@
 import logoImg from "../assets/logo.jpg";
+import { Button } from "./UI/Button";
 export function Header() {
   return (
     <header id="main-header">
       <div id="title">
         <img src={logoImg} alt="Logo" />
-        <h1>React Food</h1>
+        <h1>ReactFood</h1>
       </div>
       <nav>
-        <button>Cart (0)</button>
+        <Button textOnly>Cart (0)</Button>
       </nav>
     </header>
   );
---
+++ attachments/18 Practice Project - Food Order/01-starting-project/src/components/UI/Button.jsx
@@ -0,0 +1,8 @@
+export function Button({ children, textOnly, className, ...props }) {
+  const cssClasses = `${textOnly ? "text-button" : "button"} ${className}`;
+  return (
+    <button className={cssClasses} {...props}>
+      {children}
+    </button>
+  );
+}
