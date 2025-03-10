6e3554541e9faf2c1eacfe81250a14237277c33d
Aaron Weidele <aweidele@gmail.com>
2025-03-10 14:33:04 -0400

    19.285

--- attachments/18 Practice Project - Food Order/01-starting-project/src/components/Meals.jsx
+++ attachments/18 Practice Project - Food Order/01-starting-project/src/components/Meals.jsx
@@ -1,5 +1,6 @@
 //http://localhost:3000/meals
 import { useState, useEffect } from "react";
+import { MealItem } from "./MealItem";
 
 export function Meals() {
   const [loadedMeals, setLoadedMeals] = useState([]);
@@ -22,7 +23,7 @@ export function Meals() {
     <main>
       <ul id="meals">
         {loadedMeals.map((meal) => (
-          <li key={meal.id}>{meal.name}</li>
+          <MealItem key={meal.id} meal={meal} />
         ))}
       </ul>
     </main>
