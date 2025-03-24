470c95545cbb095ebc0bcac9406eb14e3e06fe72
Aaron Weidele <aweidele@gmail.com>
2025-03-24 15:32:55 -0400

    22.360

--- attachments/21 Routing/12-adv-starting-project/frontend/src/pages/Events.js
+++ attachments/21 Routing/12-adv-starting-project/frontend/src/pages/Events.js
@@ -1,28 +1,16 @@
-import { useEffect, useState } from "react";
-
+import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [fetchedEvents, setFetchedEvents] = useState();
-  const [error, setError] = useState();
-
-  useEffect(() => {
-    async function fetchEvents() {
-      setIsLoading(true);
-
-      setIsLoading(false);
-    }
-
-    fetchEvents();
-  }, []);
+  const events = useLoaderData();
   return (
     <>
-      <div style={{ textAlign: "center" }}>
+      <EventsList events={events} />
+      {/* <div style={{ textAlign: "center" }}>
         {isLoading && <p>Loading...</p>}
         {error && <p>{error}</p>}
       </div>
-      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />} */}
     </>
   );
 }
