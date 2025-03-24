86cf15dc3167cf1c40aec11f623d5f26523fcfa8
Aaron Weidele <aweidele@gmail.com>
2025-03-24 12:09:13 -0400

    22.348

--- attachments/21 Routing/01-starting-project/src/App.js
+++ attachments/21 Routing/01-starting-project/src/App.js
@@ -1,16 +1,22 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { HomePage } from "./pages/Home";
 import { Products } from "./pages/Products";
+import { RootLayout } from "./pages/Root";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/products", element: <Products /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <Products /> },
+    ],
+  },
 ]);
 
 function App() {
   return (
     <>
-      <h1>This is the site title</h1>
       <RouterProvider router={router} />
     </>
   );
