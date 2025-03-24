314e1a9f59efdef2c07c51341ec6761fb41f997e
Aaron Weidele <aweidele@gmail.com>
2025-03-24 13:12:18 -0400

    22.352

--- attachments/21 Routing/01-starting-project/src/App.js
+++ attachments/21 Routing/01-starting-project/src/App.js
@@ -3,6 +3,7 @@ import { HomePage } from "./pages/Home";
 import { Products } from "./pages/Products";
 import { RootLayout } from "./pages/Root";
 import { ErrorPage } from "./pages/Error";
+import { ProductDetailPage } from "./pages/ProductDetail";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +13,7 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <Products /> },
+      { path: "/products/:productId", element: <ProductDetailPage /> },
     ],
   },
 ]);
--- attachments/21 Routing/01-starting-project/src/pages/Products.js
+++ attachments/21 Routing/01-starting-project/src/pages/Products.js
@@ -1,3 +1,16 @@
+import { Link } from "react-router-dom";
+
 export function Products() {
-  return <h1>Products Page</h1>;
+  return (
+    <>
+      <h1>Products Page</h1>
+      <ul>
+        <li>
+          <Link to="/products/product-1">Product 1</Link>
+        </li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+    </>
+  );
 }
