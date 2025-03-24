92b30df5c1b0eee7eec40716d2454fc362aa1f01
Aaron Weidele <aweidele@gmail.com>
2025-03-24 15:49:10 -0400

    22.364

--- attachments/21 Routing/12-adv-starting-project/frontend/src/pages/RootLayout.jsx
+++ attachments/21 Routing/12-adv-starting-project/frontend/src/pages/RootLayout.jsx
@@ -1,13 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 export function RootLayout() {
+  const navigation = useNavigation();
   return (
     <>
       <header>
         <MainNavigation />
       </header>
       <main>
+        {navigation.state === "loading" && <p>Loading...</p>}
         <Outlet />
       </main>
     </>
