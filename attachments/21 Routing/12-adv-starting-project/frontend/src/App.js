a6149f3e9efcc7be6bc4b20710af0783d408e843
Aaron Weidele <aweidele@gmail.com>
2025-03-24 15:23:17 -0400

    22.359

--- attachments/21 Routing/12-adv-starting-project/frontend/src/App.js
+++ attachments/21 Routing/12-adv-starting-project/frontend/src/App.js
@@ -22,7 +22,7 @@
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
-import { EventsPage } from "./pages/EventsPage";
+import EventsPage from "./pages/Events";
 import { EventDetailPage } from "./pages/EventDetailPage";
 import { NewEventPage } from "./pages/NewEventPage";
 import { EditEventPage } from "./pages/EditEventPage";
