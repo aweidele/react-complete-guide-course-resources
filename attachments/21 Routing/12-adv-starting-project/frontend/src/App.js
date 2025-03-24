ec52a6e46c6f31293542a49eff0479854a06dd45
Aaron Weidele <aweidele@gmail.com>
2025-03-24 15:40:16 -0400

    22.362

--- attachments/21 Routing/12-adv-starting-project/frontend/src/App.js
+++ attachments/21 Routing/12-adv-starting-project/frontend/src/App.js
@@ -22,7 +22,7 @@
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
-import EventsPage from "./pages/Events";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import { EventDetailPage } from "./pages/EventDetailPage";
 import { NewEventPage } from "./pages/NewEventPage";
 import { EditEventPage } from "./pages/EditEventPage";
@@ -42,15 +42,7 @@ const router = createBrowserRouter([
           {
             index: true,
             element: <EventsPage />,
-            loader: async () => {
-              const response = await fetch("http://localhost:8080/events");
-              if (!response.ok) {
-                //
-              } else {
-                const resData = await response.json();
-                return resData.events;
-              }
-            },
+            loader: eventsLoader,
           },
           { path: ":eventId", element: <EventDetailPage /> },
           { path: "new", element: <NewEventPage /> },
