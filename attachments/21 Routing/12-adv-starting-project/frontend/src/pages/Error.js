fb28fdab060aa0216ff01e8cfa25fa9d26b6c708
Aaron Weidele <aweidele@gmail.com>
2025-03-25 07:18:16 -0400

    22.367

--- attachments/21 Routing/12-adv-starting-project/frontend/src/pages/Error.js
+++ attachments/21 Routing/12-adv-starting-project/frontend/src/pages/Error.js
@@ -1,3 +1,26 @@
+import { useRouteError } from "react-router-dom";
+import PageContent from "../components/PageContent";
+import MainNavigation from "../components/MainNavigation";
+
 export function ErrorPage() {
-  return <p>An error occurred</p>;
+  const error = useRouteError();
+  console.log(error);
+
+  let title = "An error occurred";
+  let message = "Something went wrong";
+
+  if (error.status === 500) message = JSON.parse(error.data).message;
+  if (error.status === 404) {
+    title = "Not found";
+    message = error.message;
+  }
+
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
 }
