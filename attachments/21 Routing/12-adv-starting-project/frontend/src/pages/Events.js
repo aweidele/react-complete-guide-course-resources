3dc4fd87070e9996a0b8b643dbdffa4c92c589c3
Aaron Weidele <aweidele@gmail.com>
2025-03-26 10:29:47 -0400

    22.379

--- attachments/21 Routing/12-adv-starting-project/frontend/src/pages/Events.js
+++ attachments/21 Routing/12-adv-starting-project/frontend/src/pages/Events.js
@@ -1,30 +1,55 @@
-import { useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
+import { Await, useLoaderData } from "react-router-dom";
+
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const data = useLoaderData();
+  const { events } = useLoaderData();
 
-  if (data.isError) {
-    return <p>{data.message}</p>;
-  }
+  return (
+    <Suspense fallback={<p>Potato?</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => {
+          console.log("loadedEvents", loadedEvents);
+          // return <p>HellO!</p>;
+          return <EventsList events={loadedEvents.events} />;
+        }}
+      </Await>
+    </Suspense>
+  );
 
-  const events = data.events;
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
+
+  // const events = data.events;
+  // return (
+  //   <>
+  //     <EventsList events={events} />
+  //   </>
+  // );
+
   return (
     <>
-      <EventsList events={events} />
+      <p>Hello</p>
     </>
   );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
   if (!response.ok) {
     // return { isError: true, message: "Could not fetch events." };
     throw new Response(JSON.stringify({ message: "Could not fetch events" }), { status: 500 });
     // throw json({ message: "Could not fetch events." }, { status: 500 });
   } else {
-    return response;
+    const resData = await response.json();
+    return resData;
   }
+}
+
+export function loader() {
+  return { events: loadEvents() };
 }
