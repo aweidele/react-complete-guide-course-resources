91781676d1caec83100c33f44fbfa87cf4b17377
Aaron Weidele <aweidele@gmail.com>
2025-08-27 01:14:21 -0400

    417

--- attachments/24 React Query/01-starting-project/src/components/Events/NewEvent.jsx
+++ attachments/24 React Query/01-starting-project/src/components/Events/NewEvent.jsx
@@ -2,6 +2,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 import { createNewEvent } from "../../util/http.js";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
+import { queryClient } from "../../util/http.js";
 
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
@@ -11,6 +12,10 @@ export default function NewEvent() {
 
   const { mutate, isPending, isError, error } = useMutation({
     mutationFn: createNewEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["events"] });
+      navigate("/events");
+    },
   });
 
   function handleSubmit(formData) {
