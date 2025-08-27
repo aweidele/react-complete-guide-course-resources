4880182900c2a27bfc6039a67204a19fafa451bc
Aaron Weidele <aweidele@gmail.com>
2025-08-26 20:36:04 -0400

    413

--- attachments/24 React Query/01-starting-project/src/components/Events/FindEventSection.jsx
+++ attachments/24 React Query/01-starting-project/src/components/Events/FindEventSection.jsx
@@ -1,10 +1,41 @@
-import { useRef } from 'react';
+import { useRef, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { fetchEvents } from "../../util/http";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import EventItem from "./EventItem.jsx";
 
 export default function FindEventSection() {
   const searchElement = useRef();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const { data, isPending, isError, error } = useQuery({ queryFn: ({ signal }) => fetchEvents({ signal, searchTerm }), queryKey: ["events", { search: searchTerm }] });
 
   function handleSubmit(event) {
     event.preventDefault();
+    setSearchTerm(searchElement.current.value);
+  }
+
+  let content = <p>Please enter a search term and to find events.</p>;
+
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = <p>{error.info?.message || "Failed to fetch events"}</p>;
+  }
+
+  if (data) {
+    content = (
+      <ul className="events-list">
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event}></EventItem>
+          </li>
+        ))}
+      </ul>
+    );
   }
 
   return (
@@ -12,15 +43,11 @@ export default function FindEventSection() {
       <header>
         <h2>Find your next event!</h2>
         <form onSubmit={handleSubmit} id="search-form">
-          <input
-            type="search"
-            placeholder="Search events"
-            ref={searchElement}
-          />
+          <input type="search" placeholder="Search events" ref={searchElement} />
           <button>Search</button>
         </form>
       </header>
-      <p>Please enter a search term and to find events.</p>
+      {content}
     </section>
   );
 }
