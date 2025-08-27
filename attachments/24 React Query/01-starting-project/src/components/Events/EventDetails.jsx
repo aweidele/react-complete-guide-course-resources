8f109b1f51ec762e01a4c622a701e2a602ebeeac
Aaron Weidele <aweidele@gmail.com>
2025-08-27 09:49:13 -0400

    419 (challenge)

--- attachments/24 React Query/01-starting-project/src/components/Events/EventDetails.jsx
+++ attachments/24 React Query/01-starting-project/src/components/Events/EventDetails.jsx
@@ -1,8 +1,45 @@
-import { Link, Outlet } from 'react-router-dom';
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
 
-import Header from '../Header.jsx';
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+
+import Header from "../Header.jsx";
+
+import { fetchEvent, deleteEvent } from "../../util/http.js";
+import { useState } from "react";
 
 export default function EventDetails() {
+  const [deleteStatus, setDeleteStatus] = useState();
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", { eventID: id }],
+    queryFn: () => fetchEvent({ id }),
+  });
+
+  const {
+    data: deleteData,
+    isPending: deleteIsPending,
+    isError: deleteIsError,
+    error: deleteError,
+  } = useMutation({
+    mutationFn: () => deleteEvent({ id }),
+    enabled: deleteStatus === "deleting",
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["events"] });
+      navigate("/events");
+    },
+  });
+
+  if (deleteIsError) setDeleteStatus("error");
+
+  const handleDelete = () => {
+    setDeleteStatus("deleting");
+  };
+
+  console.log("DATA", data);
   return (
     <>
       <Outlet />
@@ -11,25 +48,34 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
+      {isPending && <LoadingIndicator />}
+
+      {isError && <ErrorBlock title="An error occurred" message={error.info?.message || "Failed to fetch event"} />}
+
+      {data && (
+        <article id="event-details">
+          <header>
+            <h1>{data?.title}</h1>
+            <nav>
+              {deleteStatus === "deleting" ? <p>Deleting</p> : <button onClick={handleDelete}>Delete</button>}
+              <Link to="edit">Edit</Link>
+            </nav>
+            {deleteStatus === "error" && <p>Error deleting</p>}
+          </header>
+          <div id="event-details-content">
+            <img src={`http://localhost:3000/${data.image}`} alt="" />
+            <div id="event-details-info">
+              <div>
+                <p id="event-details-location">{data?.location}</p>
+                <time dateTime={`Todo-DateT$Todo-Time`}>
+                  {data?.date} @ {data?.time}
+                </time>
+              </div>
+              <p id="event-details-description">{data?.description}</p>
             </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
           </div>
-        </div>
-      </article>
+        </article>
+      )}
     </>
   );
 }
