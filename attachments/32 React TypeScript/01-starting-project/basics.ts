a009f4087a118f551dc4212ebea0d439465ec418
Aaron Weidele <aweidele@gmail.com>
2025-09-09 13:54:25 -0400

    32.521

---
+++ attachments/32 React TypeScript/01-starting-project/basics.ts
@@ -0,0 +1,11 @@
+// Primitives
+let age: number;
+let userName: string;
+let isInstructor: boolean = true;
+age = 12;
+userName = "Bob";
+
+console.log(age, userName, isInstructor);
+// Complex Types
+
+// Function types, parameters
