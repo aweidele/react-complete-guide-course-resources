31968ce9f6d882d31d8baca7f7d225d9ff4df912
Aaron Weidele <aweidele@gmail.com>
2025-09-09 16:24:09 -0400

    32.591

--- attachments/32 React TypeScript/07-react-ts-starting-project/src/App.tsx
+++ attachments/32 React TypeScript/07-react-ts-starting-project/src/App.tsx
@@ -4,7 +4,7 @@ import { Todos } from "./components/Todos";
 function App() {
   return (
     <div>
-      <Todos />
+      <Todos items={["item 1", "item 2"]} />
     </div>
   );
 }
--- attachments/32 React TypeScript/07-react-ts-starting-project/src/components/Todos.tsx
+++ attachments/32 React TypeScript/07-react-ts-starting-project/src/components/Todos.tsx
@@ -1,11 +1,11 @@
-export function Todos() {
+// Add the React Functional Component as the type for the function
+// Angled bracket plugs in a concrete value
+export const Todos: React.FC<{ items: string[] }> = (props) => {
   return (
     <ul>
-      <li>Items</li>
-      <li>Items</li>
-      <li>Items</li>
-      <li>Items</li>
-      <li>Items</li>
+      {props.items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
     </ul>
   );
-}
+};
