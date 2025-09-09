c633e8aab957689fe9d4cb7735cc681357ef7bf4
Aaron Weidele <aweidele@gmail.com>
2025-09-09 14:00:28 -0400

    32.582

--- attachments/32 React TypeScript/01-starting-project/basics.ts
+++ attachments/32 React TypeScript/01-starting-project/basics.ts
@@ -8,4 +8,22 @@ userName = "Bob";
 console.log(age, userName, isInstructor);
 // Complex Types
 
+let hobbies: string[];
+hobbies = ["Baseball", "Cooking", "Beer"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Chickenboy",
+  age: 30,
+};
+
+let people: {
+  name: string;
+  age: number;
+}[]; // An array of objects
+
 // Function types, parameters
